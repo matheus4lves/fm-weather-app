d5693c5941c7f1217d43ff4093bc290d5f65b3f5
Matheus Alves dos Santos Freitas <matheusalves.bmd@gmail.com>
2025-09-18 16:01:55 -0300

    Adapt the starter code to Next.js

---
+++ app/fonts.ts
@@ -0,0 +1,14 @@
+import { Bricolage_Grotesque, DM_Sans } from "next/font/google";
+
+export const dmSans = DM_Sans({
+  weight: ["300", "500", "600", "700"],
+});
+
+export const dmSansItalic600 = DM_Sans({
+  weight: "600",
+  style: "italic",
+});
+
+export const bricolageGrotesque = Bricolage_Grotesque({
+  weight: "700",
+});
