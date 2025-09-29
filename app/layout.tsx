d9a52c5ea8f556c9ccad302911f080187b5d90c0
Matheus Alves dos Santos Freitas <matheusalves.bmd@gmail.com>
2025-09-29 12:14:30 -0300

    Implement the Home page
    Initially, the Home page doesn't show anything except for the heading
    and search form.
    
    Then, if the location cannot be found, "No results" is displayed within
    the Home page.
    
    Finally, if one of the APIs cannot be reached, "API error" is displayed
    within the Home page.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./header";
+import { dmSans } from "./fonts";
 
 export const metadata: Metadata = {
   title: "Frontend Mentor | Weather app",
@@ -14,7 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-neutral-900">
+      <body
+        className={`bg-neutral-900 pt-200 px-200 pb-600 h-screen ${dmSans.className}`}
+      >
         <Header />
         {children}
       </body>
