b74d52bed86e6adb52d1d11edea92d3c62d4310a
Matheus Alves dos Santos Freitas <matheusalves.bmd@gmail.com>
2025-09-30 20:31:34 -0300

    Turn the search form into a component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
-import Form from "next/form";
-import Image from "next/image";
-import { bricolageGrotesque, dmSans } from "./fonts";
 
-export default function Home() {
+// Fonts
+import { bricolageGrotesque } from "./fonts";
+
+// Components
+import SearchForm from "./search-form";
+
+export default async function Page({
+
   return (
     <>
       <h1
@@ -10,29 +14,7 @@ export default function Home() {
       >
         How&apos;s the sky looking today?
       </h1>
-      <Form
-        action="/"
-        className="flex flex-col gap-3 md:flex-row md:gap-4 lg:w-164 lg:m-auto mb-150"
-      >
-        <div className="grow relative">
-          <input
-            className={`ps-15 pe-300 py-200 w-full bg-neutral-800 hover:bg-neutral-700 rounded-12 ${dmSans.className} text-neutral-0 text-preset-5 font-medium placeholder:text-neutral-200 placeholder:text-[20px] placeholder:leading-[120%] placeholder:tracking-[0] placeholder:font-medium focus:outline-2 focus:outline-neutral-0 focus:outline-offset-3`}
-            type="search"
-            name="location"
-            placeholder="Search for a place..."
-          />
-          <Image
-            className="w-250 h-250 absolute top-1/2 transform -translate-y-1/2 left-6"
-            src="/images/icon-search.svg"
-            alt="Magnifying glass icon."
-            width={21}
-            height={21}
-          />
-        </div>
-        <button className="px-300 py-200 bg-blue-500 hover:bg-blue-700 rounded-12 text-preset-5 font-medium text-neutral-0 focus:outline-2 focus:outline-blue-500 focus:outline-offset-3">
-          Search
-        </button>
-      </Form>
+      <SearchForm />
     </>
   );
 }
---
+++ app/search-form.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+import Form from "next/form";
+import Image from "next/image";
+
+// Fonts
+import { dmSans } from "./fonts";
+
+// Components
+import SearchInProgress from "./search-in-progress";
+
+export default function SearchForm() {
+  const status = useFormStatus();
+
+  return (
+    <Form
+      action=""
+      className="flex flex-col gap-3 md:flex-row md:gap-4 lg:w-164 lg:m-auto mb-600"
+    >
+      <div className="grow relative">
+        <input
+          className={`ps-15 pe-300 py-200 w-full bg-neutral-800 hover:bg-neutral-700 rounded-12 ${dmSans.className} text-neutral-0 text-preset-5 font-medium placeholder:text-neutral-200 placeholder:text-[20px] placeholder:leading-[120%] placeholder:tracking-[0] placeholder:font-medium focus:outline-2 focus:outline-neutral-0 focus:outline-offset-3`}
+          type="search"
+          name="name"
+          placeholder="Search for a place..."
+        />
+        <Image
+          className="w-250 h-250 absolute top-1/2 transform -translate-y-1/2 left-6"
+          src="/images/icon-search.svg"
+          alt="Magnifying glass icon."
+          width={21}
+          height={21}
+        />
+        {status.pending && <SearchInProgress />}
+      </div>
+      <button
+        type="submit"
+        className="px-300 py-200 bg-blue-500 hover:bg-blue-700 rounded-12 text-preset-5 font-medium text-neutral-0 focus:outline-2 focus:outline-blue-500 focus:outline-offset-3"
+      >
+        Search
+      </button>
+    </Form>
+  );
+}
