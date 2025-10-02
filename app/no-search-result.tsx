590765cbf483cb51b8010c4ffa7df33e7bdbd070
Matheus Alves dos Santos Freitas <matheusalves.bmd@gmail.com>
2025-10-02 20:29:16 -0300

    Fix NoSearchResult position

--- app/no-search-result.tsx
+++ app/no-search-result.tsx
@@ -3,7 +3,7 @@ import { dmSans } from "./fonts";
 export default function NoSearchResult() {
   return (
     <p
-      className={`text-preset-4 text-neutral-0 text-center ${dmSans.className}`}
+      className={`absolute top-43 md:top-26 w-full text-preset-4 text-neutral-0 text-center ${dmSans.className}`}
     >
       No search result found!
     </p>
