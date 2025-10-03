4cf05632b7102ccc7d178de9896e839e503f1e47
Matheus Alves dos Santos Freitas <matheusalves.bmd@gmail.com>
2025-10-02 21:50:36 -0300

    Turn the logo into a link to the Home page

--- app/header.tsx
+++ app/header.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
+
+// Components
 import UnitsDropdown from "./units-dropdown";
 import Logo from "./logo";
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between mt-200 mx-200 mb-600 ">
-      <Logo className="w-auto h-7 md:h-10" />
+      <Link href="/">
+        <Logo className="w-auto h-7 md:h-10" />
+      </Link>
       <UnitsDropdown />
     </header>
   );
