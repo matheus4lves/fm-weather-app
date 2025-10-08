9a9955cf99c3c846532e1b3150e510d27f991a74
Matheus Alves dos Santos Freitas <matheusalves.bmd@gmail.com>
2025-10-08 17:08:18 -0300

    Type the Weather Forecast API response

---
+++ app/types/weather.ts
@@ -0,0 +1,60 @@
+export type TemperatureUnit = "°C" | "°F";
+export type PrecipitationUnit = "mm" | "inch";
+export type WindSpeedUnit = "kmh" | "mph";
+
+export interface CurrentWeatherData {
+  apparent_temperature: number;
+  precipitation: number;
+  relative_humidity_2m: number;
+  temperature_2m: number;
+  time: string;
+  wind_speed_10m: number;
+}
+
+export interface CurrentWeatherUnits {
+  apparent_temperature: TemperatureUnit;
+  precipitation: PrecipitationUnit;
+  relative_humidity: "%";
+  temperature_2m: TemperatureUnit;
+  time: "iso8601";
+  wind_speed_10m: WindSpeedUnit;
+}
+
+export interface DailyForecastData {
+  temperature_2m_max: number[];
+  temperature_2m_min: number[];
+  time: string[];
+}
+
+export interface DailyForecastUnits {
+  temperature_2m_max: TemperatureUnit;
+  temperature_2m_min: TemperatureUnit;
+  time: "iso8601";
+}
+
+export interface HourlyForecastData {
+  temperature_2m: number[];
+  time: string[];
+}
+
+export interface HourlyForecastUnits {
+  temperature_2m: TemperatureUnit;
+  time: "iso8601";
+}
+
+export interface WeatherApiSuccess {
+  current?: CurrentWeatherData;
+  current_units?: CurrentWeatherUnits;
+  daily?: DailyForecastData;
+  daily_units?: DailyForecastUnits;
+  hourly?: HourlyForecastData;
+  hourly_units?: HourlyForecastUnits;
+  latitude: number;
+  longitude: number;
+  timezone: string;
+}
+
+export interface WeatherApiError {
+  error: true;
+  reason: string;
+}
