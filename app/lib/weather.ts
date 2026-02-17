a7ab48993c2a9ab853cea79791db4db8887d9010
Matheus Alves dos Santos Freitas <matheusalves.bmd@gmail.com>
2026-02-17 14:50:35 -0300

    Add missing WMO Weather interpretation codes

--- app/lib/weather.ts
+++ app/lib/weather.ts
@@ -1,6 +1,7 @@
 export function getWeatherIconSrc(weatherCode: number) {
   switch (weatherCode) {
     case 0:
+    case 1:
       return "/images/icon-sunny.webp";
     case 2:
       return "/images/icon-partly-cloudy.webp";
@@ -12,16 +13,28 @@ export function getWeatherIconSrc(weatherCode: number) {
     case 51:
     case 53:
     case 55:
+    case 56:
+    case 57:
       return "/images/icon-drizzle.webp";
     case 61:
     case 63:
     case 65:
+    case 66:
+    case 67:
+    case 80:
+    case 81:
+    case 82:
       return "/images/icon-rain.webp";
     case 71:
     case 73:
     case 75:
+    case 77:
+    case 85:
+    case 86:
       return "/images/icon-snow.webp";
     case 95:
+    case 96:
+    case 99:
       return "/images/icon-storm.webp";
     default:
       return "";
