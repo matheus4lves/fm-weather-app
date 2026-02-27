16fa9a416734011141793731d86f9693c3f72d15
Matheus Alves dos Santos Freitas <matheusalves.bmd@gmail.com>
2026-02-27 11:08:09 -0300

    fix(header): increase bottom margin on larger screens

--- app/ui/components/header.tsx
+++ app/ui/components/header.tsx
@@ -7,7 +7,7 @@ import Logo from "./logo";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between mt-200 mx-200 mb-600 ">
+    <header className="flex items-center justify-between mt-200 mx-200 mb-600 lg:mb-800">
       <Link href="/">
         <Logo className="w-auto h-7 md:h-10" />
       </Link>
