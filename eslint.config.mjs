d2605dd6c8464c88e2f04c76551bb25daa21c011
Matheus Alves dos Santos Freitas <matheusalves.bmd@gmail.com>
2026-05-19 14:38:18 -0300

    test: improve testing setup
    Add an ESLint plugin to follow best practices and anticipate common
    istakes when writing tests with jest-dom.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintPluginJestDOM from "eslint-plugin-jest-dom";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -22,6 +23,7 @@ const eslintConfig = [
     ],
   },
   eslintConfigPrettier,
+  eslintPluginJestDOM.configs["flat/recommended"],
 ];
 
 export default eslintConfig;
