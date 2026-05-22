26d0d35d8d140196d57689c3c45c838c9d70b687
Matheus Alves dos Santos Freitas <matheusalves.bmd@gmail.com>
2026-05-22 10:05:01 -0300

    test: add integration test for the home component

--- app/page.integration.test.tsx
+++ app/page.integration.test.tsx
@@ -1,9 +1,15 @@
+jest.mock("react-dom", () => ({
+  ...jest.requireActual("react-dom"),
+  useFormStatus: jest.fn(),
+}));
+
 jest.mock("next/navigation", () => ({
   ...jest.requireActual("next/navigation"),
   useSearchParams: jest.fn(),
   usePathname: jest.fn(),
 }));
 
+import { useFormStatus } from "react-dom";
 import { useSearchParams, usePathname } from "next/navigation";
 import userEvent from "@testing-library/user-event";
 import { render, screen } from "@testing-library/react";
@@ -15,6 +21,19 @@ describe("Home", () => {
   beforeEach(() => {
     (useSearchParams as jest.Mock).mockReturnValue(new URLSearchParams());
     (usePathname as jest.Mock).mockReturnValue("/");
+  });
+
+  it("renders loading state and hides previous results during a search", async () => {
+    (useFormStatus as jest.Mock).mockReturnValue({ pending: true });
+    const user = userEvent.setup();
+    render(<Home />);
+
+    const inputElement = screen.getByRole("searchbox");
+    await user.type(inputElement, "Balneário");
+    const buttonElement = screen.getByRole("button", { name: /Search/i });
+    await user.click(buttonElement);
+
+    expect(await screen.findByText(/Search in progress/i)).toBeInTheDocument();
   });
 
   it("displays a warning if the search term is too short", async () => {
